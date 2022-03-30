22398f8b0349c4bbdc3ba4cef17dcce7fe82c2cd
uiolee <22849383+uiolee@users.noreply.github.com>
2022-03-30 10:32:25 +0800

    mpy.py add `exclude`

--- mpy.py
+++ mpy.py
@@ -3,7 +3,10 @@ import os
 
 # a script run on pc to `mpy-cross` all `.py` to `.mpy` file.
 
+exclude=['boot.py','webrepl_cfg.py','mpy.py']
+
 ls = [i for i in os.listdir() if i[-3:] == '.py']
+ls =[i for i in ls if i not in exclude]
 [print(i) for i in ls]
 print('\nconut: ', len(ls))
 
